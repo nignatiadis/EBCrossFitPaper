ccdf20ea6974e5bbb227d8e515e3ae1762824ce8
Nikos Ignatiadis <nikos.ignatiadis01@gmail.com>
2019-10-26 16:40:46 -0700

    include all code reproducing synthetic experiments

--- simulations.jl
+++ simulations.jl
@@ -1,20 +1,92 @@
+using Pkg
+Pkg.activate(".")
+
 using EBayes
 using MLJ
-using XGBoost
 using Random
-
+using JLD2
 
 # Set up the MLJ Machine learning method first
-# Here we use XGBoost
+# Here we use XGBoost with iterations chosen by cross-validation
 
 MLJ.@load XGBoostRegressor
 
 tree_xgb = XGBoostRegressor(max_depth=5, eta=0.1)
-# r_num_round = range(tree, :num_round, lower=2, upper=100)
-# nested_ranges = (num_round=r_num_round, )
-#                 # gamma=r_gamma)
-# tuned_XGBoost = TunedModel(model=tree,
-#                           tuning=Grid(resolution=15),
-#                           resampling=CV(nfolds=5),
-#                           nested_ranges=nested_ranges,
-#                           measure=rms)
+r_num_round = range(tree_xgb, :num_round, lower=2, upper=100)
+
+tuned_XGBoost = TunedModel(model=tree_xgb,
+                           tuning=Grid(resolution=15),
+                           resampling=CV(nfolds=5),
+                           ranges=r_num_round,
+                           measure=rms)
+
+ebcf_tuned_xgb = EBayesCrossFit(tuned_XGBoost, num_folds=5)
+
+# Function that calculates the MSEs
+function main_simulation_eval_mse(sim::EBayes.FriedmanFayHerriotSimulation;
+                                  nreps = 200)
+   mse_vec = zeros(4, nreps)
+   for i=1:nreps
+       sim_res = rand(sim)
+       # MLE pred
+       ss = sim_res.ss
+       Xs = sim_res.X
+       true_μs = sim_res.true_μs
+
+       # Unbiased/MLE prediction
+       pred_unbiased = response(ss)
+       mse_vec[1, i] = mean( (pred_unbiased .- true_μs).^2 )
+
+       # SURE prediction
+       fit_sure = fit(Normal(), SURE(), GrandMeanLocation(), ss)
+       pred_sure = predict(fit_sure)
+       mse_vec[3, i] = mean( (pred_sure .- true_μs).^2 )
+
+       # XGBoost and EBCF prediction
+       fit_ebcf_reg = fit(ebcf_tuned_xgb, Xs, ss, verbosity=1)
+       pred_ebcf = predict(fit_ebcf_reg)
+       pred_reg = fit_ebcf_reg.reg_preds
+
+       mse_vec[2, i] = mean( (pred_reg .- true_μs).^2 )
+       mse_vec[4, i] = mean( (pred_ebcf .- true_μs).^2 )
+    end
+    #order in mse_vec is ["Unbiased" "XGBoost" "SURE" "EBCF"]
+    (all_res = mse_vec, mses = mean(mse_vec; dims=2),
+     mse_ses = std(mse_vec; dims=2)./sqrt(nreps),
+     A_sqrt = sim.A_sqrt, n = sim.n, p = sim.p, σ = sim.σ)
+ end
+
+
+#-----------------------------
+# Simulation settings
+#------------------------------
+
+# MSE calculated by averaging 100 Monte Carlo replicates
+nreps = fill(100,15)
+# For n=300 realizations more noisy, so average 400 Monte carlo replicates
+nreps[1:3] .= 400
+
+
+ns = Int.(collect(range(300; stop=2000, length=5)))
+p = 15
+σ = 2.0
+As_sqrt = [0.0; 2.0; 3.0]
+
+sim_param_combs = vec([(n,A_sqrt) for A_sqrt in As_sqrt, n in ns])
+
+
+#tst_fh = EBayes.FriedmanFayHerriotSimulation(1000, 5 , 0.5, 1.0)
+#tmp = main_simulation_eval_mse(tst_fh; nreps=3)
+
+
+for i in 1:length(sim_param_combs)
+    tupl = sim_param_combs[i]
+    @show tupl
+    n = tupl[1]
+    A_sqrt = tupl[2]
+    sim_fh = EBayes.FriedmanFayHerriotSimulation(n, p , A_sqrt, σ)
+    benchmark_res = main_simulation_eval_mse(sim_fh; nreps=nreps[i])
+    @JLD2.save "synthetic_benchmark_files/benchmark_res_$(i).jld2" benchmark_res
+ end
+
+
--- synthetic_benchmark_plots.jl
+++ synthetic_benchmark_plots.jl
@@ -1,3 +1,6 @@
+using Pkg
+Pkg.activate(".")
+
 using FileIO
 using JLD2
 using Plots
@@ -10,32 +13,115 @@ sim_df = DataFrame(A_sqrt = Float64[],
                    p=Int64[],
                    σ=Float64[],
                    n=Int64[],
-                   Unbiased=Float64[],
-                   xgboost=Float64[],
-                   SURE=Float64[],
-                   EBCF = Float64[])
+                   mse=Float64[],
+                   mse_se=Float64[],
+                   method = String[])
 
-i = 1
-to_load =  "synthetic_benchmark_files/benchmark_res_$(i).jld2"
-tmp_res = FileIO.load(to_load,"benchmark_res")
 
 methods_text = ["Unbiased" "XGBoost" "SURE" "EBCF"]
 method_to_idx = Dict(["Unbiased" "XGBoost" "SURE" "EBCF"] .=> [1 2 3 4])
 
-res1[2]
-tmp_df = DataFrame(A_sqrt = tmp_res[:A_sqrt],
+for i=1:15
+  to_load =  "synthetic_benchmark_files/benchmark_res_$(i).jld2"
+  tmp_res = FileIO.load(to_load,"benchmark_res")
+  tmp_df = DataFrame(A_sqrt = tmp_res[:A_sqrt],
              p=tmp_res[:p],
              σ=tmp_res[:σ],
              n=tmp_res[:n],
              mse=vec(tmp_res[:mses]),
              mse_se=vec(tmp_res[:mse_ses]),
              method=vec(methods_text))
+  append!(sim_df, tmp_df)
+end
 
-wide_df = unstack(tmp_df, [:A_sqrt,:p, :σ, :n], :method, :mse)
+#-------------------------------------
+# Maximum standard errors
+#-------------------------------------
+
+rmse_ci_lhs = sqrt.(sim_df[:,:mse] .- 1 .* sim_df[:,:mse_se])
+rmse_ci_rhs = sqrt.(sim_df[:,:mse] .+ 1 .* sim_df[:,:mse_se])
+
+ci_widths = rmse_ci_rhs .- rmse_ci_lhs
+se_proxies = ci_widths ./ 2
+se_delta = sim_df[:,:mse_se] ./ (2 .* sqrt.(sim_df[:,:mse]))
+
+maximum(se_proxies)
+maximum(se_delta)
+
+#-------------------------------------
+# Preparations towards plotting
+#-------------------------------------
+
+wide_df = unstack(sim_df, [:A_sqrt,:p, :σ, :n], :method, :mse)
+
+sub_df = DataFrames.groupby(wide_df, :A_sqrt)
+
+upscale = 0.7#8 #8x upscaling in resolution
+default(size=(400*upscale,350*upscale))
+
+#-------------------------------------
+#      Figure 2, panel c (A_sqrt = 3)
+#-------------------------------------
+
+
+sub_df_c = sub_df[3]
+sim_plot_np_panel_c = plot(sub_df_c[:,:n],
+                sqrt.(Matrix(sub_df_c[:, Symbol.(vec(methods_text))])),
+                seriestype=:line,
+                xlim=(300,2000),
+                ylim=(0.0,4.2),
+                color = ["grey" "orange" "blue" "black"],
+                linestyle = [:dashdotdot :dashdot :dot :dash],
+                label=methods_text,
+                xlab=L"$n$",
+                xticks = [300,1100,1900],
+                yticks = [0, 2, 4],
+                grid=false,
+                legend=:bottomright,
+                ylab=L"E[( \hat{\mu}_i - \mu_i)^2]^{\frac{1}{2}}")
+
+savefig(sim_plot_np_panel_c, "simulation_panel_c.pdf")
+
+#-------------------------------------
+#      Figure 2, panel a (A_sqrt = 0)
+#-------------------------------------
+
+sub_df_a = sub_df[1]
+sim_plot_np_panel_a= plot(sub_df_a[:,:n],
+                sqrt.(Matrix(sub_df_a[:, Symbol.(vec(methods_text))])),
+                seriestype=:line,
+                xlim=(300,2000),
+                ylim=(0.0,4.2),
+                color = ["grey" "orange" "blue" "black"],
+                linestyle = [:dashdotdot :dashdot :dot :dash],
+                label="",
+                xlab=L"$n$",
+                xticks = [300,1100,1900],
+                yticks = [0, 2, 4],
+                grid=false,
+                legend=nothing,
+                ylab=L"E[( \hat{\mu}_i - \mu_i)^2]^{\frac{1}{2}}")
 #
-#for (n, A_sqrt) in eb_params
-#     file1 = ebayes_sim_filename(n, A_sqrt)
-#     res1 = FileIO.load(file1,"bench_res")
-#     res1 = res1[2]
-#     push!(sim_df, (A_sqrt, n, res1...))
-# end
+savefig(sim_plot_np_panel_a, "simulation_panel_a.pdf")
+
+#-------------------------------------
+#      Figure 2, panel b (A_sqrt = 2)
+#-------------------------------------
+
+sub_df_b = sub_df[2]
+sim_plot_np_panel_b= plot(sub_df_b[:,:n],
+                sqrt.(Matrix(sub_df_b[:, Symbol.(vec(methods_text))])),
+                seriestype=:line,
+                xlim=(300,2000),
+                ylim=(0.0,4.2),
+                color = ["grey" "orange" "blue" "black"],
+                linestyle = [:dashdotdot :dashdot :dot :dash],
+                label="",
+                xlab=L"$n$",
+                xticks = [300,1100,1900],
+                yticks = [0, 2, 4],
+                grid=false,
+                legend=nothing,
+                ylab=L"E[( \hat{\mu}_i - \mu_i)^2]^{\frac{1}{2}}")
+#
+savefig(sim_plot_np_panel_b, "simulation_panel_b.pdf")
